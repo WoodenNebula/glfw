d07f919bc0f943faf2b1768fc5a0c02ca7309d4f
WoodenNebula <87257970+WoodenNebula@users.noreply.github.com>
2025-10-13 16:51:32 +0545

    update glfw.lua to support linux build

--- glfw.lua
+++ glfw.lua
@@ -7,17 +7,35 @@ project "GLFW"
 	targetdir ("%{wks.location}/build/bin/%{wks.outputdir}/%{prj.name}")
     objdir ("%{wks.location}/build/obj/%{wks.outputdir}/%{prj.name}")
 
+	vpaths {
+		["Headers"] = { "**.h"},
+		["Sources/*"] = { "**.cpp", "**.c"},
+	}
 
 	files
 	{
 		"include/GLFW/glfw3.h",
 		"include/GLFW/glfw3native.h",
+		
 		"src/glfw_config.h",
+
+		"src/internal.h",
+		"src/mappings.h",
+		
+		"src/null_joystick.h",
+		"src/null_platform.h",
+
+		"src/platform.h",
+	}
+
+	files
+	{
 		"src/context.c",
+
 		"src/init.c",
 		"src/input.c",
 		"src/monitor.c",
-
+		
 		"src/null_init.c",
 		"src/null_joystick.c",
 		"src/null_monitor.c",
@@ -35,24 +53,45 @@ project "GLFW"
 		
 		files
 		{
+			"src/posix_module.c",
+			"src/posix_time.h",
+			"src/posix_time.c",
+			"src/posix_thread.h",
+			"src/posix_thread.c",
+			"src/posix_poll.h",
+			"src/posix_poll.c",
+
+			"src/glx_context.c",
+			"src/linux_joystick.h",
+			"src/linux_joystick.c"
+		}
+
+	-- filter {"system:linux", "backend:X11"}
+		files
+		{		
 			"src/x11_init.c",
 			"src/x11_monitor.c",
 			"src/x11_window.c",
 			"src/xkb_unicode.c",
-			"src/posix_module.c",
-			"src/posix_time.c",
-			"src/posix_thread.c",
-			"src/posix_module.c",
-			"src/glx_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c",
-			"src/linux_joystick.c"
 		}
 
 		defines
 		{
 			"_GLFW_X11"
 		}
+	-- filter {"system:linux", "backend:WAYLAND"}
+	-- 	files
+	-- 	{		
+	-- 		"src/wl_platform.h",
+	-- 		"src/wl_init.c",
+	-- 		"src/wl_monitor.c",
+	-- 		"src/wl_window.c",
+	-- 	}
+
+	-- 	defines
+	-- 	{
+	-- 		"_GLFW_WAYLAND"
+	-- 	}
 
 	filter "system:macosx"
 		pic "On"
@@ -81,21 +120,32 @@ project "GLFW"
 
 		files
 		{
+			"src/win32_joystick.h",
+			"src/win32_platform.h",
+			"src/win32_thread.h",
+			"src/win32_time.h",
+		}
+
+		files 
+		{
+			"src/egl_context.c",
+			"src/wgl_context.c",
+			"src/osmesa_context.c",
+
 			"src/win32_init.c",
 			"src/win32_joystick.c",
 			"src/win32_module.c",
 			"src/win32_monitor.c",
-			"src/win32_time.c",
 			"src/win32_thread.c",
+			"src/win32_time.c",
 			"src/win32_window.c",
-			"src/wgl_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c"
 		}
 
 		defines 
 		{ 
 			"_GLFW_WIN32",
+			"_UNICODE",
+			"UNICODE",
 			"_CRT_SECURE_NO_WARNINGS"
 		}
 
